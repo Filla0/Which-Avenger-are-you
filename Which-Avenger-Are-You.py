5d8f57d93e772c905af22b0490e101aac719d952
Filimon Haftu <filimonhaftu18@gmail.com>
2023-04-12 16:34:34 +0200

    Create Which-Avenger-Are-You.py

---
+++ Which-Avenger-Are-You.py
@@ -0,0 +1,28 @@
+print("Welcome to 'Which Avenger are you?' quiz game!")
+print("Please answer the following questions:")
+
+score = 0
+
+q1 = input("What is your favorite color?\n(a) Red\n(b) Blue\n(c) Green\n(d) Black\n")
+if q1 == "a":
+    score += 1
+elif q1 == "b":
+    score += 2
+elif q1 == "c":
+    score += 3
+elif q1 == "d":
+    score += 4
+else:
+    print("invalid answer.")
+
+q2 = input("What is your preferred weapon?\n(a) Hammer\n(b) Shield\n(c) Bow and Arrow\n(d) Guns\n")
+if q2 == "a":
+    score += 4
+elif q2 == "b":
+    score += 3
+elif q2 == "c":
+    score += 2
+elif q2 == "d":
+    score += 1
+else:
+    print("invalid answer.")        
